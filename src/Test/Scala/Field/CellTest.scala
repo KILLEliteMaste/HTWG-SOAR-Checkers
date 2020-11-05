9ea3f691c8cc9590365ed754868846d67e33746b
KILLEliteMaste <d.fellbaum@hotmail.de>
2020-11-05 14:17:54 +0100

    First test

---
+++ src/Test/Scala/Field/CellTest.scala
@@ -0,0 +1,34 @@
+package Field
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class CellTest extends AnyWordSpec with Matchers {
+  "A Cell" when {
+    "not set to any value " should {
+      val emptyCell = Cell(0)
+      println("HIER")
+      "have value 0" in {
+        emptyCell.value should be(0)
+      }
+      "not be set" in {
+        emptyCell.isSet should be(false)
+      }
+      "show unplayable mark" in {
+        emptyCell.toString.matches("/s{3}")
+      }
+    }
+    "set to a specific value" should {
+      val nonEmptyCell = Cell(5)
+      "return that value" in {
+        nonEmptyCell.value should be(5)
+      }
+      "be set" in {
+        nonEmptyCell.isSet should be(true)
+      }
+      "show number in String" in {
+        nonEmptyCell.toString.matches("▐ 5 ▐")
+      }
+    }
+  }
+}
