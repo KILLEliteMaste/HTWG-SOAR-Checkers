226f057f4968630ed4bf8f2369f157f1c86c0099
Aron Lapp <Aron.Lapp@t-online.de>
2020-11-06 12:35:22 +0100

    input String is split to access matrix with more ease

--- src/main/scala/aview/TUI.scala
+++ src/main/scala/aview/TUI.scala
@@ -4,10 +4,10 @@ import Field._
 class TUI {
 
   def processInputLine(input: String, field:Field): Field = {
-    //input = input.split(" ").toList
-      input match {
-        case "n" => new Field(10)
-        case "q" => field
+    val inputsplit = input.split("\\s+").toList
+      inputsplit match {
+        case "new" :: "field" :: Nil => new Field(10)
+        case "quit" :: "game" :: Nil => field
       }
   }
 }
