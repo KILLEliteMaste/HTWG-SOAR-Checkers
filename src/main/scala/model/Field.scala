a809142131fa6b32496d4f91b43a15cbb42b67a2
KILLEliteMaste <d.fellbaum@hotmail.de>
2020-11-13 12:50:30 +0100

    Adjusted field toString to display numbers on the edge

--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -1,6 +1,8 @@
-package Field
+package model
 
 case class Field(fieldSize: Int) {
+
+  var statusString: String =""
 
   def totalFieldSize: Int = fieldSize * fieldSize
 
@@ -11,16 +13,16 @@ case class Field(fieldSize: Int) {
       if (index < 4) {
         //Fill the rows for white
         if (index % 2 == 0) {
-          builder.+=(Vector.tabulate(fieldSize)(n => if (n % 2 == 0) Cell(0) else Cell(1)))
-        } else {
           builder.+=(Vector.tabulate(fieldSize)(n => if (n % 2 == 0) Cell(1) else Cell(0)))
+        } else {
+          builder.+=(Vector.tabulate(fieldSize)(n => if (n % 2 == 0) Cell(0) else Cell(1)))
         }
       } else if (fieldSize - 3 < index) {
         //Fill the rows for black
         if (index % 2 == 0) {
-          builder.+=(Vector.tabulate(fieldSize)(n => if (n % 2 == 0) Cell(0) else Cell(3)))
-        } else {
           builder.+=(Vector.tabulate(fieldSize)(n => if (n % 2 == 0) Cell(3) else Cell(0)))
+        } else {
+          builder.+=(Vector.tabulate(fieldSize)(n => if (n % 2 == 0) Cell(0) else Cell(3)))
         }
       } else {
         //Empty row
@@ -31,8 +33,15 @@ case class Field(fieldSize: Int) {
   }
 
   override def toString: String = {
-    var output = ""
+    var str = "  "
+    for (i <- 0 until fieldSize) {
+      str += "   " + i
+    }
+    var output = str + "\n"
+    var counter = 0
     for (row <- matrix.rows) {
+      output += counter + "  "
+      counter = counter + 1
       for (cell <- row) {
         output += cell
       }
